123c90d32ebbd367aa111d6920eb72f2e6bf0f54
brukk <bruk1892@gmail.com>
2018-03-01 21:03:20 -0700

    javascript object of shopper added to exercises

---
+++ exercises/groceryStore/grocery.js
@@ -0,0 +1,9 @@
+var shopper={
+    dress:"cotton",
+    shoeSize:10,
+    married:true,
+    appearance: function(){
+        return this.dress + "" + this.shoeSize;
+    },
+    groceryCart:["tommato","meat","vegi"]
+}

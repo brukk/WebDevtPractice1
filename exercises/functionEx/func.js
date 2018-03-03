b53c812555145e7b6966506ab6239aa64521392f
brukk <bruk1892@gmail.com>
2018-03-03 00:34:17 -0700

    css exercise using semantic tags added

---
+++ exercises/functionEx/func.js
@@ -0,0 +1,23 @@
+function adding(x,y){
+    return 2+4;
+}
+adding()
+
+
+ var largeN =function(x,y,z){
+     
+     return Math.max(x,y,z);
+ }
+ console.log(largeN(4,6,7))
+
+
+
+ var evenN = function(n) {
+     if (n % 2 === 0){
+  console.log("even") 
+ }
+ console.log("odd") 
+}
+evenN(9);
+
+

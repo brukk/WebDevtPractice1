a20a56d4a6b0ae3127af91c6bf3f96bb7515fc4e
brukk <bruk1892@gmail.com>
2018-03-20 00:06:06 -0600

    responsive color boxes using css flex box

--- exercises/djJs/dj.js
+++
@@ -1,6 +0,0 @@
-var butten1=document.getElementById("button1");
-butten1.addEventListener("click", colorchange)
-function colorchange(){
-    butten1.style.backgroundcolor="red";
-}
-var body=document.getElementsByTagName("body")
--- project/mario/mario.js
+++ project/mario/mario.js
@@ -1,15 +1,22 @@
 var form = document.getElementById("submit");
-var zGoo = document.getElementById("goo");
+var zGoo = document.getElementById("goo").innerHTML;
 var zBob = document.getElementById("bob");
 var zCheap = document.getElementById("cheap");
 var total = document.getElementById("total");
 
 var data=0;
 
-zGoo.addEventListener("input", setPrice); 
-zBob.addEventListener("input", setPrice); 
-zCheap.addEventListener("input", setPrice); 
+zGoo.addEventListener("input", function(setPrice){
+    data=setPrice*5;
+    alert (data);
+}); 
+// form.addEventListener("submit", function () {
     
-function setPrice(){
-    var one=
-}
+// event.target.value;
+
+// zBob.addEventListener("input", setPrice); 
+// zCheap.addEventListener("input", setPrice); 
+    
+// function multi(){
+
+// }
